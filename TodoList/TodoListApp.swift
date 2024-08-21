3b0abd7a141838449b1622f66ec3214f0ffde43e
kaviarasanmani <kavikavi41@rocketmail.com>
2024-08-21 11:50:18 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by kavi17311 on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
